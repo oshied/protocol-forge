7877d184100d4fa451fd52b972396c799eec72d2
Kevin Carter <kevin.carter@figment.io>
2022-10-25 06:58:19 -0500

    test deb
    Signed-off-by: Kevin Carter <kevin.carter@figment.io>

--- .github/bin/build-deb.sh
+++ .github/bin/build-deb.sh
@@ -6,8 +6,13 @@ export BUILD_VERSION="${DEB_VERSION:-0.0.0-$VERSION}"
 export BUILD_NAME="$(basename ${PROTOCOL_NAME} | sed 's/.git//g')"
 export BUILD_ARCH="$(dpkg --print-architecture)"
 
-sed -i "s/REPLACE_NAME/${BUILD_NAME}/g" /dpgk/DEBIAN/control
-sed -i "s/REPLACE_VERSION/${BUILD_VERSION}/g" /dpgk/DEBIAN/control
-cp -R /mnt/* /dpgk/
-dpkg-deb --build --root-owner-group /dpgk
-cp /dpgk.deb "/packages/${BUILD_NAME}_${CONTAINER_TAG}_${BUILD_ARCH}.deb"
+mkdir /mnt/DEBIAN
+for item in control postinst postrm; do
+  sed -e "s/REPLACE_NAME/${BUILD_NAME}/g" -e "s/REPLACE_VERSION/${BUILD_VERSION}/g" "/dpgk/DEBIAN/${item}" > "/mnt/DEBIAN/${item}"
+done
+
+chmod +x /mnt/DEBIAN/postinst
+chmod +x /mnt/DEBIAN/postrm
+
+dpkg-deb --build --root-owner-group /mnt
+cp /mnt.deb "/packages/${BUILD_NAME}_${CONTAINER_TAG}_${BUILD_ARCH}.deb"
